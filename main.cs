96d731f96811a195a6cda00e880b62bd7ec31ee6
Katherine Smith <katherinecharlotte116@gmail.com>
2020-09-30 20:01:36 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,108 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    //I should have asked before about the first part of this lab but, it says to do it outside the loop. This is the only code that I could come up with that is outside the loop. 
+    
+    Console.WriteLine("What is the number of the first car?");
+    int maxCounter = Convert.ToInt32(Console.ReadLine());
+
+    for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#1");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+      for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#2");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken by the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+    for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#3");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken by the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+    for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#4");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken by the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+    for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#5");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken by the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+    for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#6");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken by the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+    for(int counter=1;counter<=maxCounter;counter++)
+    {
+    Console.WriteLine("Enter the distance covered by Car#7");
+    double distance = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Enter the time taken by the Car");
+    double time = Convert.ToDouble(Console.ReadLine()); 
+    double speed = distance/time;
+    Console.WriteLine("The speed of this car is " + speed + " miles per hour ");
+    }
+
+    Console.WriteLine("The End");
+
+
+
+
+
+    
+
+   
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+    
+  
+
+  }
+}
